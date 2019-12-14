aafc3568f4d3ee3a5da92b57f24921eb50012916
vibraniumforge <gregm9999@gmail.com>
2019-12-14 16:01:31 -0700

    added index route

--- backend/app/controllers/api/v1/trains_controller.rb
+++ backend/app/controllers/api/v1/trains_controller.rb
@@ -1,9 +1,9 @@
 module Api::V1
-class TrainsController < ApplicationController
+  class TrainsController < ApplicationController
 
-  def index
-    render json: Train.all
+    def index
+      render json: Train.all
+    end
+
   end
-
-end
 end
