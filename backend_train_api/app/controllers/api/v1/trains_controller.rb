01ea56c6454545f8cdcbf2ae58a4d779cb7c9e6a
vibraniumforge <gregm9999@gmail.com>
2020-01-02 22:08:40 -0800

    2 routes

--- backend_train_api/app/controllers/api/v1/trains_controller.rb
+++ backend_train_api/app/controllers/api/v1/trains_controller.rb
@@ -11,7 +11,7 @@ module Api::V1
     def show
       if @train.nil?
         puts "Train not found"
-        render json: { message: "Train NOT found.", success: false }, status: 406
+        render json: { message: "Train NOT found.", success: false, data: @train.errors.full_messages }, status: 406
       else
         puts "success"
         render json: { message: "Train successfully returned.", success: true, data: @train }, status: 200
@@ -29,7 +29,7 @@ module Api::V1
       else
         puts "Train not saved"
         puts "Errors= #{@train.errors.full_messages.join(", ")}"
-        render json: { message: "Train NOT saved because #{@train.errors.full_messages.join(", ")}", success: false }, status: 406
+        render json: { message: "Train NOT saved because #{@train.errors.full_messages.join(", ")}", success: false, data: @train.errors.full_messages }, status: 406
       end
     end
         
@@ -46,18 +46,39 @@ module Api::V1
         render json: { message: "Train successfully saved.", success: true, data: @train }, status: 200
       else
         puts "Train not saved"
-        puts "Errors= #{@train.errors.full_messages.join(", ")}"
         render json: { message: "Train NOT updated because #{@train.errors.full_messages.join(", ")}", success: false }, status: 406
+        puts "Errors= #{@train.errors.full_messages.join(", ")}"
       end
     end
-
+    
     def destroy
       if @train.destroy
         render json: { message: "Train successfully deleted.", success: true, data: @train }, status: 200
       else
-        puts "Error in delete: #{@train.errors.full_messages.join(", ")}"
-        puts @train.errors.full_messages.join(", ")
         render json: { message: "Train NOT successfully deleted.", success:false, data: @train.errors.full_messages.join(", ") }, status: 406
+        puts "Error in delete: #{@train.errors.full_messages.join(", ")}"
+      end
+    end
+
+    def amtrak_station
+      response = Faraday.get("http://dixielandsoftware.net/Amtrak/solari/data/#{params[:id]}_schedule.php?data=#{params[:id]}") 
+      body = JSON.parse(response.body)
+      if response.success? 
+        @trains = body["response"]
+        render json: {message: "Amtrak info found.", success: true, data: @trains}, status: 200
+      else 
+        render json: {message: "Amtrak info not found.", success:false, data: @train.errors }, status: 406
+      end
+    end
+
+    def amtrak_station_search
+      response = Faraday.get("http://www.dixielandsoftware.net/cgi-bin/station_search.pl?data=#{params[:id]}") 
+      if response.success? 
+        @stations = response.body
+        # render json: {message: "Amtrak info found.", success: true, data: @stations }, status: 200
+        render plain: @stations
+      else 
+        render json: {message: "Amtrak info not found.", success:false, data: @stations.errors.full_messages }, status: 406
       end
     end
 
