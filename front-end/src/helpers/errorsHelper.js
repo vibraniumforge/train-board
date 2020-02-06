7b9faed5db19fade0c195f55e56054b5458ed785
vibraniumforge <gregm9999@gmail.com>
2020-02-05 22:19:01 -0800

    first commit

---
+++ front-end/src/helpers/errorsHelper.js
@@ -0,0 +1,30 @@
+const errorsHelper = () => {
+  console.log("errorsHelper fires");
+
+  // const value = inputValue.trim();
+  // const formField = inputFormField.trim();
+  // switch (formField) {
+  //   case "destination":
+  //     return vinputValuealue.length === 0 ? "errors" : "";
+  //   case "newtime":
+  //     return ![0, 4].includes(inputValue.length) ? "errors" : "";
+  //   case "newtime24":
+  //     return ![0, 4].includes(inputValue.length) ? "errors" : "";
+  //   case "origin":
+  //     return inputValue.length === 0 ? "errors" : "";
+  //   case "remarks_boarding":
+  //     return inputValue.length === 0 ? "errors" : "";
+  //   case "scheduled":
+  //     return inputValue.length !== 4 ? "errors" : "";
+  //   case "scheduled24":
+  //     return inputValue.length !== 4 ? "errors" : "";
+  //   case "service":
+  //     return inputValue.length === 0 ? "errors" : "";
+  //   case "trainno":
+  //     return inputValue.length === 0 ? "errors" : "";
+  //   default:
+  //     return "";
+  // }
+};
+
+export { errorsnHelper };
