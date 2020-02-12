1377b6c6014710461ddff3b97300a775d960c9d2
vibraniumforge <gregm9999@gmail.com>
2020-02-12 14:34:27 -0800

    refactor

--- front-end/src/helpers/remarksHelper.js
+++ front-end/src/helpers/remarksHelper.js
@@ -1,8 +1,6 @@
 const remarksHelper = remark => {
   const trimmedRemark = remark.trim();
-  if (trimmedRemark.indexOf("E") > -1) {
-    return "green";
-  }
+
   switch (trimmedRemark) {
     case "S. Disrupt":
       return "Service Disrupt";
